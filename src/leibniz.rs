523ec609fc3cda51d6217e23342cefaa5f98a2b0
Erick Cestari <erickcestari03@gmail.com>
2024-02-04 14:06:50 -0300

    fix: Clarifying comment of warning cpu usage

--- src/leibniz.rs
+++ src/leibniz.rs
@@ -4,7 +4,7 @@ pub fn leibniz_pi() {
     let iterations: u64 = 1000000000;
 
     // Set the maximum number of threads to 4 (you can adjust this number) 
-    // !Warning: It can overheat your CPU use a low value on num_threads
+    // !Warning: It may overheat your CPU, use a low value in num_threads
     rayon::ThreadPoolBuilder::new().num_threads(4).build_global().unwrap();
 
     let pi_estimate = (0..iterations)
